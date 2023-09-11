5b8081b0f6f416b443dea02f1b9dd076c7ddd724
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-09-11 11:50:44 -0300

    refact: :dizzy: Separação do componente de formulário

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,42 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+import { useContext } from 'react'
+import { CyclesContext } from '../..'
+import { useFormContext } from 'react-hook-form'
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em: </label>
+      <TaskInput
+        {...register('task')}
+        id="task"
+        type="text"
+        placeholder="Dê um nome para o seu projeto"
+        list="tasks-suggestions"
+        disabled={!!activeCycle}
+      />
+      <datalist id="tasks-suggestions">
+        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Projeto 3" />
+      </datalist>
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        {...register('minutesAmount', { valueAsNumber: true })}
+        id="minutesAmount"
+        type="number"
+        placeholder="00"
+        step={5}
+        min={5}
+        max={60}
+        maxLength={2}
+        disabled={!!activeCycle}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
