b5854c1dfc4a69d1e8104364d7c8a4b857155b00
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-09-13 09:54:15 -0300

    docs: (reducer.ts) :open_file_folder: Renomeando para reducer

---
+++ src/reducers/Cycles/reducer.ts
@@ -0,0 +1,53 @@
+import { ActionTypesEnum } from '../../enums/actions-enum'
+
+export interface CycleProps {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+  interruptedDate?: Date
+  finishedDate?: Date
+}
+
+interface CyclesState {
+  cycles: CycleProps[]
+  activeCycleId: string | null
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function cyclesReducer(state: CyclesState, action: any) {
+  switch (action.type) {
+    case ActionTypesEnum.ADD_NEW_CYCLE:
+      return {
+        ...state,
+        cycles: [...state.cycles, action.payload.newCycle],
+        activeCycleId: action.payload.newCycle.id,
+      }
+    case ActionTypesEnum.INTERRUPT_CURRENT_CYCLE:
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, interruptedDate: new Date() }
+          } else {
+            return cycle
+          }
+        }),
+        activeCycleId: null,
+      }
+    case ActionTypesEnum.MARK_CURRENT_CYCLE_AS_FINISHED:
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, finishedDate: new Date() }
+          } else {
+            return cycle
+          }
+        }),
+        activeCycleId: null,
+      }
+    default:
+      return state
+  }
+}
